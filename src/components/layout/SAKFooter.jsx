7da3c84421eb3a20f06e3eb39dd4ccfbb889c41f
Mark SEMAKULA™ <marksemakula@gmail.com>
2025-12-29 21:40:30 +0300

    Update SAKGH footer

--- src/components/layout/SAKFooter.jsx
+++ src/components/layout/SAKFooter.jsx
@@ -16,7 +16,7 @@ const campuses = [
 
 const SAKFooter = () => {
   return (
-    <footer className="text-white pt-10 pb-8" style={{ backgroundColor: '#708090' }}>
+    <footer className="text-white pt-10 pb-8" style={{ backgroundColor: '#3C4242' }}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
           <div className="space-y-3">
